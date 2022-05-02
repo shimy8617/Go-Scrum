dff5d47e180216fa330f473fc3ca08b614099605
shimy8617 <cristinashim@yahoo.com.ar>
2022-05-02 15:34:43 -0300

    me

--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -1,3 +1,88 @@
 import { Header } from "../../Header/Header";
+import "./Task.styles.css";
 
-export const Tasks = () => <Header />;
+export const Tasks = () => (
+<><Header />
+<main id="tasks">
+    <section className="wrapper_list">
+        <div className="list_header"><h2>Mis tareas</h2>
+        </div>
+        <div className="list_group">escritorio</div>
+    <div className="list-phone">
+        <div className="card">
+            <div className="close">x</div>
+                <h2>Tarea 1</h2>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Cristina Shim</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción fake</p>
+        </div>
+        <div className="card">
+            <div className="close">x</div>
+                <h2>Tarea 1</h2>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Cristina Shim</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción fake</p>
+        </div>
+        <div className="card">
+            <div className="close">x</div>
+                <h2>Tarea 1</h2>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Cristina Shim</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción fake</p>
+        </div>
+        <div className="card">
+            <div className="close">x</div>
+                <h2>Tarea 1</h2>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Cristina Shim</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción fake</p>
+        </div>
+        <div className="card">
+            <div className="close">x</div>
+                <h2>Tarea 1</h2>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Cristina Shim</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción fake</p>
+        </div>
+        <div className="card">
+            <div className="close">x</div>
+                <h2>Tarea 1</h2>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Cristina Shim</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción fake</p>
+        </div>
+        <div className="card">
+            <div className="close">x</div>
+                <h2>Tarea 1</h2>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Cristina Shim</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción fake</p>
+        </div>
+        <div className="card">
+            <div className="close">x</div>
+                <h2>Tarea 1</h2>
+                <h6>24/1/2022 16:40hs</h6>
+                <h5>Cristina Shim</h5>
+                <button type="button">Nueva</button>
+                <button type="button">Alta</button>
+                <p>Descripción fake</p>
+        </div>
+    </div>    
+    </section>
+</main>;
+</>
+)
