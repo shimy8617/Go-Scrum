7bcc51f5fa9d12b456ada96f63ba669e3a28a263
shimy8617 <cristinashim@yahoo.com.ar>
2022-05-04 22:11:18 -0300

    home

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,7 +1,7 @@
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 import "./TaskForm.styles.css";
-
 
 export const TaskForm = () => {
   const initialValues = {
@@ -15,41 +15,59 @@ export const TaskForm = () => {
     alert();
   };
 
-  const formik = useFormik({ initialValues, onSubmit });
+  const required = "* Campo obligatotio";
 
-  const { handleSubmit, handleChange } = formik;
+  const validationSchema = () =>
+    Yup.object().shape({
+      title: Yup.string()
+        .min(6, "La cantidad mínima de caracteres es 6")
+        .required(required),
+      status: Yup.string().required(required),
+      priority: Yup.string().required(required),
+    });
 
-    return( 
+  const formik = useFormik({ initialValues, validationSchema, onSubmit });
+
+  const { handleSubmit, handleChange, errors } = formik;
+
+  return (
     <section className="task-form">
-        <h2>Crear tarea</h2>
-        <p>Crea tus tareas</p>
-        <form onSubmit = {handleSubmit}>
-            <div>
-                <div>
-                    <imput name="title" onChange={handleChange} placeholder="Título" />
-                </div>
-                <div>
-                    <select name="status" onChange={handleChange}>
-                        <option value="">Seleccionar opción</option>
-                        <option value="new">Nueva</option>
-                        <option value="inProcess">En proceso</option>
-                        <option value="finished">Terminada</option>
-                    </select>
-                </div>
-                <div>
-                    <select name="priority" onChange={handleChange}>
-                        <option value="">Seleccionar opción</option>
-                        <option value="low">Baja</option>
-                        <option value="medium">Media</option>
-                        <option value="high">Alta</option>
-                    </select>
-                </div>
-            </div>
-            <div>
-                <textarea name="description" onChange={handleChange} placeholder="Descripción" />
-            </div>
-            <button type="submit">Crear</button>
-        </form>
+      <h2>Crear tarea</h2>
+      <p>Crea tus tareas</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            <input name="title" onChange={handleChange} placeholder="Título" />
+          </div>
+          {errors.title && <span>{errors.title}</span>}
+          <div>
+            <select name="status" onChange={handleChange}>
+              <option value="">Seleccionar opción</option>
+              <option value="new">Nueva</option>
+              <option value="inProcess">En proceso</option>
+              <option value="finished">Terminada</option>
+            </select>
+          </div>
+          {errors.status && <span>{errors.status}</span>}
+          <div>
+            <select name="priority" onChange={handleChange}>
+              <option value="">Seleccionar opción</option>
+              <option value="low">Baja</option>
+              <option value="medium">Media</option>
+              <option value="high">Alta</option>
+            </select>
+          </div>
+          {errors.priority && <span>{errors.priority}</span>}
+        </div>
+        <div>
+          <textarea
+            name="description"
+            onChange={handleChange}
+            placeholder="Descripción"
+          />
+        </div>
+        <button type="submit">Crear</button>
+      </form>
     </section>
-    )
-}
+  );
+};
