ee128d7879b90e7c41d01e2887e5b9e06637cc08
Cristina Shim <84110240+shimy8617@users.noreply.github.com>
2022-05-09 17:40:51 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./App.css";
 import { Login } from "./components/views/auth/Login/Login";
 import { Register } from "./components/views/auth/Register/Register";
 import { Tasks } from "./components/views/Tasks/Tasks";
+import Registered from "./components/views/Registered/Registered";
 
 const Error404 = lazy(() => import("./components/views/Error404/Error404"));
 
@@ -60,6 +61,20 @@ export const App = () => {
               variants={pageTransition}
             >
               <Login />
+            </motion.div>
+          }
+        />
+<Route
+          path="/registered/:teamID"
+          element={
+            <motion.div
+              className="page"
+              initial="out"
+              animate="in"
+              exit="out"
+              variants={pageTransition}
+            >
+              <Registered />
             </motion.div>
           }
         />
