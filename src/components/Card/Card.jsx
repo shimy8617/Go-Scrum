e0ee19aa45d60e8b5bda2ac023778dc05334f5e8
shimy8617 <cristinashim@yahoo.com.ar>
2022-05-10 11:03:08 -0300

    me

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,13 @@
 export const Card = ({
-  data: { title, datetime, creator, description, type, priority },
+  data: { title, createdAt, user: {userName}, description, status, importance },
 }) => (
   <div className="card">
     <div className="close">x</div>
     <h2>{title}</h2>
-    <h6>{datetime}</h6>
-    <h5>{creator}</h5>
-    <button type="button">{type}</button>
-    <button type="button">{priority}</button>
+    <h6>{createdAt}</h6>
+    <h5>{userName}</h5>
+    <button type="button">{status}</button>
+    <button type="button">{importance}</button>
     <p>{description}</p>
   </div>
 );
