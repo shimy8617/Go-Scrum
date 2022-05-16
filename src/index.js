b2046acbdd49da0c6dcdefe827d7b576fd79a721
shimy8617 <cristinashim@yahoo.com.ar>
2022-05-16 14:03:40 -0300

    me

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import {Provider} from 'react-redux';
 
 //Styles
 import "./index.css";
+import {store} from "./store/store";
 import { App } from "./App";
 
 render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ src/store/reducers/tasksReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    loading: false,
+    tasks: [],
+    error: ""
+}
+
+export const tasksReducer = (state = initialState) => {
+    return state;
+    
+}
